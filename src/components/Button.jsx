173b55210246eb35800b7dd3611a6bacabbf075b
enotdgk <enotdgk@mail.com>
2021-06-14 10:39:07 +0300

    simple route to cart

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classNames from 'classnames'
+
+function Button({ onClick, className, outline, children}) {
+    return (
+        <button
+            onClick={onClick}
+            className={classNames('button', className, {
+                'button--outline' : outline
+        })}>
+            {children || 'Button'}
+        </button>
+    )
+}
+
+export default Button
